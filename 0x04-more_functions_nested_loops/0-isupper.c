86de3335e963729d0d5d32e581b5a4a377d9ca18
Taiye <ooomoleye@ymail.com>
2022-10-23 16:27:30 -0700

    Initial

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,19 +5,15 @@
  * @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
+
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = 0;
-
-	for (; uppercase <= 'Z'; uppercase++)
+	if (c >= 65 && c <= 90)
 	{
-	if (c == uppercase)
+		return (1);
+	}
+	else
 	{
-	isupper = 1;
-	break;
+		return (0);
 	}
-	}
-
-	return (isupper);
 }
