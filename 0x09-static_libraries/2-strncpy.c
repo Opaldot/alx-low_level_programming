62b43c5e8e82927c3d979c4dfcec480b3b74c6cd
Opaldot <ooomoleye@ymail.com>
2022-11-08 23:47:22 -0800

    correct of libmy.a

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strncpy - copies a string up to n
+* @dest: Destination of the pointer to the string
+* @src: Source of the pointer to the string to copy
+* @n: Number of bytes to copy.
+* Return: Pointer to the destination string.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+        int e;
+
+        for (e = 0; src[e] != '\0' && e < n; e++)
+                dest[e] = src[e];
+        while (e < n)
+                dest[e++] = '\0';
+        return (dest);
+}
