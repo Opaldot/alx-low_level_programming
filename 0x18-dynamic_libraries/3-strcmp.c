393e2aea91309735c50b8909802e75c2536b2c63
Opaldot <ooomoleye@ymail.com>
2023-01-18 19:00:58 -0800

    libdynamic

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings, ending at the first byte that is different.
+* @s1: string to be compared
+* @s2: string to compare by
+* Return: the difference between the first character that are not the same
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int e;
+
+	for (e = 0; s1[e] != '\0' && s2[e] != '\0' && s1[e] == s2[e]; e++)
+		;
+	return (s1[e] - s2[e]);
+}
